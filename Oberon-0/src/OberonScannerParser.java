24bb51e7c3af102087cba76097a2e7df224ebb7c
Maurice Knoop <mpjwknoop@gmail.com>
2016-05-20 21:14:54 +0200

    Used more examples

--- Oberon-0/src/OberonScannerParser.java
+++ Oberon-0/src/OberonScannerParser.java
@@ -15,13 +15,13 @@ public class OberonScannerParser {
 
     public static void main(String[] args) {
 
-        try(BufferedReader reader = new BufferedReader(new FileReader("Oberon-0/examples/simple.oberon0"))) {
+        try(BufferedReader reader = new BufferedReader(new FileReader("examples/given.oberon0"))) {
 
             OberonScanner previewScanner = new OberonScanner(reader);
 
             Symbol token = previewScanner.nextToken();
             while (token.getId() != OberonParser.Terminals.EOF) {
-                System.out.println(token.getId());
+                //System.out.println(token.getId());
                 token = previewScanner.nextToken();
             }
         } catch (beaver.Scanner.Exception e) {
@@ -29,22 +29,26 @@ public class OberonScannerParser {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("Done! ");
 
-        try(BufferedReader reader = new BufferedReader(new FileReader("Oberon-0/examples/simple.oberon0"))) {
-            OberonScanner scanner = new OberonScanner(reader);
+        String[] files = new String[]{"examples/error_1.oberon0", "examples/error_2.oberon0", "examples/error_3.oberon0", "examples/error_4.oberon0"};
+        String[] expectedUnexpectedToken = new String[]{"32", "CONSTANT", "", ""};
+        for(int i = 0 ; i  <  files.length; ++i){
+            System.out.println("Testing: "+files[i]);
+            try (BufferedReader reader = new BufferedReader(new FileReader(files[i]))) {
+                OberonScanner scanner = new OberonScanner(reader);
 
-            OberonParser parser = new OberonParser();
+                OberonParser parser = new OberonParser();
 
-            parser.parse(scanner);
+                parser.parse(scanner);
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Parser.Exception e) {
-            e.printStackTrace();
+            } catch (Parser.Exception e) {
+                System.out.println("Expected unexpected token: "+expectedUnexpectedToken[i]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Tested: "+files[i]);
         }
-
     }
 
 }
